6923604aff1a5e2090c8eeed8c5d4418149b2e3d
sungeunp <sungeun1214@gmail.com>
2025-05-12 18:14:39 +0900

    Apply nextjs/image component to ArtCard component

--- src/components/ArtCard/index.tsx
+++ src/components/ArtCard/index.tsx
@@ -1,5 +1,6 @@
 import "@/components/ArtCard/ArtCard.css";
 import { Art_thumbnail, Art } from "@/types/Art";
+import Image from "next/image";
 import Link from "next/link";
 
 const ArtCard = (props: {
@@ -9,12 +10,21 @@ const ArtCard = (props: {
   return (
     <Link href={`/art/${props.data.id}`}>
       <figure className="art">
-        <img
+        <Image
           src={props.data.imageUrls[0]}
+          layout="raw"
+          width={200}
+          height={200}
           className="rounded-xl bg-gray-200"
           alt={props.data.imageUrls[0]}
           title={props.data.id}
         />
+        {/* <img
+          src={props.data.imageUrls[0]}
+          className="rounded-xl bg-gray-200"
+          alt={props.data.imageUrls[0]}
+          title={props.data.id}
+        /> */}
       </figure>
     </Link>
   );
