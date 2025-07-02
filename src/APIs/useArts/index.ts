78a3245613e5ef2539712308f2b55cc9a04a08d7
sungeunp <sungeun1214@gmail.com>
2025-07-02 10:00:19 +0900

    [FEAT] Update API PATH
    API server 변경에 따른 수정
    /api/posts/list -> /api/posts

--- src/APIs/useArts/index.ts
+++ src/APIs/useArts/index.ts
@@ -5,7 +5,7 @@ export type sortType = "LATEST" | "HOTTEST" | "FOLLOWING";
 const useArts = (sort: sortType, page: number, size: number) => {
   console.log(`${process.env.NEXT_PUBLIC_API_HOST}`);
   return useSWR(
-    `${process.env.NEXT_PUBLIC_API_HOST}/api/posts/list?sort=${sort || "LATEST"}&page=${page || 0}&size=${size || 20}`,
+    `${process.env.NEXT_PUBLIC_API_HOST}/api/posts?sort=${sort || "LATEST"}&page=${page || 0}&size=${size || 20}`,
     (url: string) => fetch(url).then((res) => res.json())
   );
 };
