baec48792b465ecf1d10b5b097c9d83065524e6a
sungeunp <sungeun1214@gmail.com>
2025-08-24 18:42:53 +0900

    [CHORE] API response update에 따른 로직 수정
    및 thumbnail url 로직 분기 추가 (추후 통합됨)

--- src/components/ArtCard/index.tsx
+++ src/components/ArtCard/index.tsx
@@ -1,22 +1,27 @@
-import { Art_thumbnail, Art } from "@/types/Art";
+import { Art_thumbnail, Art, Art_thumbnail_profiles } from "@/types/Art";
 import Image from "next/image";
 import Link from "next/link";
 
-const ArtCard = (props: {
+const ArtCard = ({
+  data,
+}: {
   // type: "thumbnail" | "full";
-  data: Art | Art_thumbnail;
+  data: Art | Art_thumbnail | Art_thumbnail_profiles;
 }) => {
+  const { postId, images, imageUrl } = data;
+  const safeThumbnailUrl =
+    images && images.length > 0 ? images[0].imageUrl : imageUrl;
   return (
-    <Link href={`/art/${props.data.id}`}>
+    <Link href={`/art/${postId}`}>
       <figure>
         <Image
-          src={props.data.images[0].imageUrl}
-          alt={props.data.images[0].imageUrl}
+          src={safeThumbnailUrl}
+          alt={safeThumbnailUrl}
           // layout="fill"
           width={200}
           height={200}
           className="rounded-xl bg-gray-200 w-full"
-          title={String(props.data.id)}
+          title={String(postId)}
         />
       </figure>
     </Link>
