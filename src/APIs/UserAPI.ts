c821fcc9cdd856d3d74eb8495cac9294e1d380d5
sungeunp <sungeun1214@gmail.com>
2025-09-21 20:36:36 +0900

    Improves user data handling and error resilience
    Enhances API error handling for user profile retrieval, preventing application crashes due to API errors such as 401.
    
    Adds the login path to the paths where the footer is hidden.
    
    Adds missing user profile properties to the User type definition.

--- src/APIs/UserAPI.ts
+++ src/APIs/UserAPI.ts
@@ -5,23 +5,26 @@ export const getUserMe = async (token: string): Promise<UserMe | null> => {
     // cancel request
     return null;
   }
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_HOST}/api/profiles/me`,
-    {
+  const res = await (
+    await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/profiles/me`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
+    })
+  ).json();
+
+  const { isSuccess, status, data } = res;
+  if (!isSuccess) {
+    switch (status) {
+      case 401: // 만료됨?
+        return null;
+      default:
+        return null;
     }
-  );
-  if (!res.ok) {
-    // 에러 처리
-    throw new Error("Failed to fetch user me info");
+    // throw new Error("Failed to load user info");
   }
-  const custom_response = await res.json();
-  if (!custom_response.isSuccess) {
-    throw new Error("Failed to load user info");
-  }
-  const user = custom_response.data;
+
+  const user = data;
   return user;
 };
 
