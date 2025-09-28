9020366b88f859d5377a3e5bc781f3652415c379
sungeunp <sungeun1214@gmail.com>
2025-09-28 13:58:16 +0900

    [CHORE] fix

--- src/APIs/UserAPI.ts
+++ src/APIs/UserAPI.ts
@@ -1,31 +1,25 @@
 import { User, UserMe } from "@/types/User";
-import _fetch from "./fetcher/ServerSide";
+import serverFetch from "./fetcher/ServerSide";
 
-export const getUserMe = async (token: string): Promise<UserMe | null> => {
-  if (!token) {
-    // cancel request
-    return null;
-  }
+export const getUserMe = async (): Promise<UserMe | null> => {
   const res = await (
-    await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/profiles/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    await serverFetch(
+      `${process.env.NEXT_PUBLIC_API_HOST}/api/profiles/me`,
+      true
+    )
   ).json();
 
-export const getUserMe_client = async (): Promise<UserMe | null> => {
-  const res = await clientFetch(
-    `${process.env.NEXT_PUBLIC_API_HOST}/api/profiles/me`,
-    true
-  );
-  if (!res.ok) {
-    // 에러 처리
-    throw new Error("Failed to fetch user me info");
+  const { isSuccess, status, data } = res;
+  if (!isSuccess) {
+    switch (status) {
+      case 401: // 만료됨?
+        return null;
+      default:
+        return null;
+    }
+    // throw new Error("Failed to load user info");
   }
-
-  const user = data;
-  return user;
+  return data;
 };
 
 export const getUser = async (userId: string): Promise<User | null> => {
