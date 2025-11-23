dce7ed3e8a1332c0cdfe04265c404044d5547d94
sungeunp <sungeun1214@gmail.com>
2025-11-23 21:50:21 +0900

    [FEAT] 로그아웃 기능 구현

---
+++ src/APIs/auth/logout/index.ts
@@ -0,0 +1,24 @@
+import { APIFailureMsg, TITLE_500 } from "@/APIs";
+import _fetch from "@/APIs/fetcher/ClientSide";
+
+const logout = async (): Promise<boolean | APIFailureMsg> => {
+  const response = await (
+    await _fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/auth/logout`, true, {
+      method: "POST",
+    })
+  ).json();
+  console.log("response :>> ", response);
+  const { isSuccess, status } = response;
+  if (!isSuccess) {
+    switch (status) {
+      case 404:
+        return { code: 404, title: "이미 로그아웃된 세션입니다." };
+      default:
+        return { code: 500, title: TITLE_500 };
+    }
+  }
+
+  return true;
+};
+
+export default logout;
